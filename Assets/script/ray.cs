f248fd9dc5f07ed0c2a463db1e20ffa40a0b4799
PCnorby22 <bigleite10@gmail.com>
2025-04-29 16:54:30 -0300

    gustavo leite

--- Assets/script/ray.cs
+++ Assets/script/ray.cs
@@ -1,22 +1,35 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ray : MonoBehaviour
 {
-    public float rayDistance = 10f; // Distância do Raycast
-    public LayerMask layerMask;     // (Opcional) Define em quais layers o Raycast pode acertar
-
+    public float rayDistance = 10f;
+    public LayerMask layerMask;
+    public Slider poder;
+    float t = 0;
+    private void Start()
+    {
+        poder.value = 0;
+    }
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        t += Time.fixedDeltaTime;
+        if (t >= 10 && poder.value <=poder.maxValue)
         {
+            poder.value++;
+            t = 0;
+        }
+        if (Input.GetKey(KeyCode.Space) && poder.value >= poder.maxValue)
+        {
+            poder.value = 0;
             Ray ray = new Ray(transform.position, transform.right);
             RaycastHit hit;
-
-            if (Physics.Raycast(ray, out hit, rayDistance, layerMask))
+            if (Physics.Raycast(ray, out hit))
             {
-                // Destroi o objeto atingido
-                Destroy(hit.collider.gameObject);
-                Debug.Log("Objeto destruído: " + hit.collider.gameObject.name);
+                if (hit.collider.gameObject.tag == "obistaculo")
+                {
+                    Destroy(hit.collider.gameObject);
+                }
             }
         }
     }
